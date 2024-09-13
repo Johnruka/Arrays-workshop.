e50e65ac823b0574007197c3b09a39759d31d536
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-13 11:25:01 +0200

    modified

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+
+        int[] size = new int[]{5, 6, 5, 8, 9, 4};
+        System.out.println(size);
+    }
+    }
+
+
+
+
+
+
